51e937c83cf173ae1e42359f49e0f32d9b705f5f
DESKTOP-GGAJ1JF\601-11 <kimjw62@nate.com>
2024-04-15 16:53:10 +0900

    dev-게시판-hit-20240415

--- src/main/java/edu/sejong/ex/mapper/BoardMapper.java
+++ src/main/java/edu/sejong/ex/mapper/BoardMapper.java
@@ -9,10 +9,16 @@ import edu.sejong.ex.vo.BoardVO;
 @Mapper
 public interface BoardMapper {
 	List<BoardVO> getList();
+
 	BoardVO read(int bno);
+	
+	public void updateHit(int bno);		//업데이트 : hit + 1
+//	int updateHit(BoardVO boardVO);
+	
 	int delete(int bid);
 	int insertBoard(BoardVO boardVO);
 	int updateBoard(BoardVO boardVO);
-	public void updateShape(BoardVO boardVO);
+	
+	public void updateShape(BoardVO boardVO);	//댓글-답변
 	public void insertReply(BoardVO boardVO);
 }
--- src/main/java/edu/sejong/ex/service/BoardServiceImpl.java
+++ src/main/java/edu/sejong/ex/service/BoardServiceImpl.java
@@ -27,9 +27,20 @@ public class BoardServiceImpl implements BoardService {
 	public BoardVO get(int bno) {
 		log.info("get()...");
 		
+		boardMapper.updateHit(bno);
+		
+//		BoardVO board = new BoardVO();
+//		board.setBid(bno);
+//		updateHit(board);
+		
 		return boardMapper.read(bno);
 	}
 
+//	public void updateHit(BoardVO board) {
+//		log.info("updateHit()...");
+//		boardMapper.updateHit(board);
+//	}
+	
 	@Override
 	public int remove(int bid) {
 		log.info("remove()...");
