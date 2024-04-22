e902f6328002743638a403f4230bd246a3ff1f28
DESKTOP-GGAJ1JF\601-11 <kimjw62@nate.com>
2024-04-22 16:59:32 +0900

    dev-시큐리티-20240422

--- src/main/java/edu/sejong/ex/controller/HomeController.java
+++ src/main/java/edu/sejong/ex/controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import edu.sejong.ex.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 //@RequestMapping("/index")
 public class HomeController {
@@ -21,15 +23,14 @@ public class HomeController {
 	public String shopList(Model model) {
 		System.out.println("shopList()...");
 		model.addAttribute("deptEmps", empService.getDeptEmpList());
-		return "/emp/index";
+		return "index";
+	}
+	@GetMapping("/")
+	public String home(Model model) {
+		log.info("home()...");
+		model.addAttribute("deptEmps", empService.getDeptEmpList());
+		return "home";
 	}
 
-//	@PostMapping("/write")
-//	public String write(UserVO userVO) {
-//		System.out.println("write()...");
-//		userService.insertUser(userVO);
-//		userService.insertAuthorities(userVO);
-//		return "redirect:/emp/index";
-//	}
 
 }
